9907f9eaaa10e9ca15d4620c22e41e386b385a1a
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-06 19:56:05 +0600

    practice some react native components

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <StatusBar></StatusBar>
+      <Text style={{color:"red"}}>I am a developer, and i'm try to learn react native app</Text>
+      {/* <Image source={require("./assets/adaptive-icon.png")} style={{hight:200, width:200}} /> */}
+      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, magnam, eligendi voluptates totam impedit non aspernatur esse repellendus hic nisi laborum culpa minus mollitia fuga quam amet, architecto eveniet maxime!</Text>
+      <Text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores adipisci officia quas repellendus quibusdam id saepe laboriosam ab possimus nemo hic, tenetur rerum reiciendis autem incidunt. Est incidunt dolores molestias.</Text>
+      <Image source={require("./assets/adaptive-icon.png")} style={{height:400, width:300}}></Image>
+      <Image source={require("./assets/adaptive-icon.png")} style={{height:400, width:300}}></Image>
+      <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={{height:400, width:350}} />
+      
+      
+      </ScrollView>
     </View>
   );
 }
@@ -16,5 +26,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:30,
+    padding:10,
   },
 });
