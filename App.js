f1c1980996fee74c7cb7d9eb9e3f78425e933ce0
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-07 23:28:33 +0600

    Assignment 1

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ScrollView>
         <StatusBar></StatusBar>
-      <Text style={{color:"red"}}>I am a developer, and i'm try to learn react native app</Text>
-      {/* <Image source={require("./assets/adaptive-icon.png")} style={{hight:200, width:200}} /> */}
-      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, magnam, eligendi voluptates totam impedit non aspernatur esse repellendus hic nisi laborum culpa minus mollitia fuga quam amet, architecto eveniet maxime!</Text>
-      <Text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores adipisci officia quas repellendus quibusdam id saepe laboriosam ab possimus nemo hic, tenetur rerum reiciendis autem incidunt. Est incidunt dolores molestias.</Text>
-      <Image source={require("./assets/adaptive-icon.png")} style={{height:400, width:300}}></Image>
-      <Image source={require("./assets/adaptive-icon.png")} style={{height:400, width:300}}></Image>
-      <Image source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} style={{height:400, width:350}} />
-      
-      
+        <View style={styles.View1}>
+          <Image
+            source={require("./assets/favicon.png")}
+            style={styles.image}
+          />
+          <Text style={styles.text}> The Octocat</Text>
+          <Text style={styles.text2}> Octocat</Text>
+          <Text style={styles.text3}> Joined 25 jan 2011</Text>
+        </View>
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {},
+  View1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:30,
-    padding:10,
+    marginTop: 35,
+    backgroundColor: "red",
+    flexDirection: "row",
   },
+  image: {},
+  text: {flexDirection:'column'},
+  text2: {flexDirection:'column'},
+  text3: {flexDirection:'column'},
 });
